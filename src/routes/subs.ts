f52836fb28f5ba3a8ead6c162821fa1c6c21d672
*k52VZy3ygq <dk.kho@rsinteractive.co.kr>
2023-02-01 17:36:07 +0900

    sub_add_fin

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,18 +1,33 @@
 import { NextFunction, Request, Response, Router } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { User } from "../entity/User";
+import Sub from "../entity/Sub";
 import authMiddleware from "../middlewares/auth";
 import userMiddleware from "../middlewares/user";
 
 const router = Router();
 
 const createSub = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("a", req.body);
   const { name, title, description } = req.body;
-  console.log("createsub");
-  // try{
 
-  // }
+  try {
+    const comunityName = await Sub.findOneBy({ name });
+    if (comunityName) {
+      return res
+        .status(400)
+        .json({ name: "이미 사용 중인 커뮤니티 이름입니다." });
+    }
+
+    const sub = new Sub();
+    sub.name = name;
+    sub.title = title;
+    sub.description = description;
+    sub.user = res.locals.user;
+    await sub.save();
+    return res.json(sub);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ e });
+  }
 };
 
 router.post("/", userMiddleware, authMiddleware, createSub);
